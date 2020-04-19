ea411617605b14d1dcc1a5255efdd8b454b5a004
Tiunchik <Senebh@gmail.com>
2020-04-19 21:31:54 +0300

    0. Видео[#249865] [#00002]

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # job4j_car - The learning task to work with spring.
 
+[![Build Status](https://travis-ci.org/Tiunchik/job4j_car.svg?branch=master)](https://travis-ci.org/Tiunchik/job4j_car)
+
---
+++ src/main/java/spring/context/StartUI.java
@@ -0,0 +1,43 @@
+/**
+ * Package spring.context for
+ *
+ * @author Maksim Tiunchik
+ */
+package spring.context;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Class StartUI -
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 19.04.2020
+ */
+public class StartUI {
+
+    private static final Logger LOG = LogManager.getLogger(StartUI.class.getName());
+
+    private final Store store;
+
+    private final ConsoleInput input;
+
+    public StartUI(Store store, ConsoleInput input) {
+        this.store = store;
+        this.input = input;
+    }
+
+    public void add(String question) {
+        String value = input.askUsr(question);
+        if (value != null) {
+            store.add(value);
+        }
+    }
+
+    public void print() {
+        for (String value : store.getAll()) {
+            System.out.println(value);
+        }
+    }
+}
